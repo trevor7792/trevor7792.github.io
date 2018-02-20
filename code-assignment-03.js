8a1be50aef0cc54186ce4cef63f6c1e5d09b8acb
tpwarner7792 <tpwarner7792@gmail.com>
2018-02-20 01:27:50 -0600

    code 3 and book assignment 3a

---
+++ code-assignment-03.js
@@ -0,0 +1,8 @@
+let myNumber = 8
+let myString = 'Night Out'
+let myBoolean = true
+let myArray = ['shamrocks', 'bars', 'beers']
+console.log('My Special Number:' + myNumber)
+console.log('Text String:' + myString)
+console.log('Boolean Statement:' + myBoolean)
+console.log('Array of words for assignment:' + myArray)
