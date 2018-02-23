8511ddc991e2f763d8d3a8af67676be4f761c367
tpwarner7792 <tpwarner7792@gmail.com>
2018-02-22 20:01:20 -0600

    book assignment 5 and code 4

---
+++ code-assignment-04.js
@@ -0,0 +1,26 @@
+let projectArray = [
+  1,
+  'https://trevor7792.github.io',
+  'Trevors Website',
+  'Shamrocks and Shenanigans',
+  true,
+  ['shamrocks', 'shenanigans', 'beers']
+]
+
+let projectObject = {
+  id: 1,
+  url:'https://trevor7792.github.io',
+  title: 'Trevors Website',
+  desc: 'Shamrocks and Shenanigans',
+  thumb: true,
+  keywords: [
+    'shamrocks',
+    'shenanigans',
+    'beers'
+  ]
+}
+
+
+console.log(projectArray[2])
+console.log(projectObject.title)
+console.log(projectObject["Trevors Website"])
